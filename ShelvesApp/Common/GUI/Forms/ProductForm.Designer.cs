21c03b43aa365c921f37d40986afede32d760cc7
Francisco Javier Becerra-Ortiz <francisco@fjbo.net>
2019-04-21 22:44:01 -0700

    Implemented [funky] "responsive" GUI and fixed some GUI errors.

--- ShelvesApp/Common/GUI/Forms/ProductForm.Designer.cs
+++ ShelvesApp/Common/GUI/Forms/ProductForm.Designer.cs
@@ -31,14 +31,21 @@
 			this.ResetActionButton = new Shelves.GUI.Controls.ActionButton();
 			this.SaveActionButton = new Shelves.GUI.Controls.ActionButton();
 			this.ProductDataPanel = new Shelves.App.Common.GUI.Controls.ProductDataPanel();
+			this.CrudPanelsFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+			this.InventoryPartsCrudPanel = new Shelves.App.Common.GUI.Controls.PartsCrudPanel();
+			this.AssociatedPartsCrudPanel = new Shelves.App.Common.GUI.Controls.PartsCrudPanel();
+			this.FlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
 			this.ActionsPanel.SuspendLayout();
 			this.mainPanel.SuspendLayout();
+			this.CrudPanelsFlowLayoutPanel.SuspendLayout();
+			this.FlowLayoutPanel.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// ActionsPanel
 			// 
 			this.ActionsPanel.Controls.Add(this.SaveActionButton);
 			this.ActionsPanel.Controls.Add(this.ResetActionButton);
+			this.ActionsPanel.Size = new System.Drawing.Size(1184, 50);
 			this.ActionsPanel.Controls.SetChildIndex(this.CloseButton, 0);
 			this.ActionsPanel.Controls.SetChildIndex(this.ResetActionButton, 0);
 			this.ActionsPanel.Controls.SetChildIndex(this.SaveActionButton, 0);
@@ -47,12 +54,18 @@
 			// 
 			this.CloseButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
 			this.CloseButton.FlatAppearance.BorderSize = 0;
+			this.CloseButton.Location = new System.Drawing.Point(1113, 8);
+			// 
+			// headerPanel
+			// 
+			this.headerPanel.Size = new System.Drawing.Size(1184, 90);
 			// 
 			// mainPanel
 			// 
-			this.mainPanel.Controls.Add(this.ProductDataPanel);
+			this.mainPanel.Controls.Add(this.FlowLayoutPanel);
+			this.mainPanel.Size = new System.Drawing.Size(1184, 451);
 			this.mainPanel.Controls.SetChildIndex(this.ActionsPanel, 0);
-			this.mainPanel.Controls.SetChildIndex(this.ProductDataPanel, 0);
+			this.mainPanel.Controls.SetChildIndex(this.FlowLayoutPanel, 0);
 			// 
 			// ResetActionButton
 			// 
@@ -78,7 +91,7 @@
 			this.SaveActionButton.FlatAppearance.BorderSize = 0;
 			this.SaveActionButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.SaveActionButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.SaveActionButton.Location = new System.Drawing.Point(668, 8);
+			this.SaveActionButton.Location = new System.Drawing.Point(1052, 8);
 			this.SaveActionButton.Name = "SaveActionButton";
 			this.SaveActionButton.Size = new System.Drawing.Size(55, 30);
 			this.SaveActionButton.TabIndex = 4;
@@ -89,12 +102,58 @@
 			// ProductDataPanel
 			// 
 			this.ProductDataPanel.BackColor = System.Drawing.Color.Transparent;
-			this.ProductDataPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.ProductDataPanel.Location = new System.Drawing.Point(0, 0);
-			this.ProductDataPanel.Margin = new System.Windows.Forms.Padding(6);
+			this.ProductDataPanel.Location = new System.Drawing.Point(9, 8);
+			this.ProductDataPanel.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
 			this.ProductDataPanel.Name = "ProductDataPanel";
-			this.ProductDataPanel.Size = new System.Drawing.Size(800, 401);
+			this.ProductDataPanel.Size = new System.Drawing.Size(350, 262);
 			this.ProductDataPanel.TabIndex = 1;
+			// 
+			// CrudPanelsFlowLayoutPanel
+			// 
+			this.CrudPanelsFlowLayoutPanel.AutoScroll = true;
+			this.CrudPanelsFlowLayoutPanel.Controls.Add(this.InventoryPartsCrudPanel);
+			this.CrudPanelsFlowLayoutPanel.Controls.Add(this.AssociatedPartsCrudPanel);
+			this.CrudPanelsFlowLayoutPanel.Location = new System.Drawing.Point(3, 281);
+			this.CrudPanelsFlowLayoutPanel.Name = "CrudPanelsFlowLayoutPanel";
+			this.CrudPanelsFlowLayoutPanel.Size = new System.Drawing.Size(801, 392);
+			this.CrudPanelsFlowLayoutPanel.TabIndex = 2;
+			// 
+			// InventoryPartsCrudPanel
+			// 
+			this.InventoryPartsCrudPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+			this.InventoryPartsCrudPanel.DisplaySearchbox = true;
+			this.InventoryPartsCrudPanel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+			this.InventoryPartsCrudPanel.ItemType = null;
+			this.InventoryPartsCrudPanel.Location = new System.Drawing.Point(4, 4);
+			this.InventoryPartsCrudPanel.Margin = new System.Windows.Forms.Padding(4);
+			this.InventoryPartsCrudPanel.Name = "InventoryPartsCrudPanel";
+			this.InventoryPartsCrudPanel.Size = new System.Drawing.Size(383, 380);
+			this.InventoryPartsCrudPanel.TabIndex = 0;
+			this.InventoryPartsCrudPanel.Text = "All Parts";
+			// 
+			// AssociatedPartsCrudPanel
+			// 
+			this.AssociatedPartsCrudPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+			this.AssociatedPartsCrudPanel.DisplaySearchbox = false;
+			this.AssociatedPartsCrudPanel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+			this.AssociatedPartsCrudPanel.ItemType = null;
+			this.AssociatedPartsCrudPanel.Location = new System.Drawing.Point(395, 4);
+			this.AssociatedPartsCrudPanel.Margin = new System.Windows.Forms.Padding(4);
+			this.AssociatedPartsCrudPanel.Name = "AssociatedPartsCrudPanel";
+			this.AssociatedPartsCrudPanel.Size = new System.Drawing.Size(383, 380);
+			this.AssociatedPartsCrudPanel.TabIndex = 1;
+			this.AssociatedPartsCrudPanel.Text = "Associated Parts";
+			// 
+			// FlowLayoutPanel
+			// 
+			this.FlowLayoutPanel.AutoScroll = true;
+			this.FlowLayoutPanel.Controls.Add(this.ProductDataPanel);
+			this.FlowLayoutPanel.Controls.Add(this.CrudPanelsFlowLayoutPanel);
+			this.FlowLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.FlowLayoutPanel.Location = new System.Drawing.Point(0, 0);
+			this.FlowLayoutPanel.Name = "FlowLayoutPanel";
+			this.FlowLayoutPanel.Size = new System.Drawing.Size(1184, 401);
+			this.FlowLayoutPanel.TabIndex = 3;
 			// 
 			// ProductForm
 			// 
@@ -102,12 +161,16 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.CloseButton;
-			this.ClientSize = new System.Drawing.Size(800, 541);
+			this.ClientSize = new System.Drawing.Size(1184, 541);
 			this.Name = "ProductForm";
 			this.Text = "ProductForm";
+			this.Load += new System.EventHandler(this.ProductForm_Load);
+			this.Resize += new System.EventHandler(this.ProductForm_Resize);
 			this.ActionsPanel.ResumeLayout(false);
 			this.ActionsPanel.PerformLayout();
 			this.mainPanel.ResumeLayout(false);
+			this.CrudPanelsFlowLayoutPanel.ResumeLayout(false);
+			this.FlowLayoutPanel.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
@@ -117,5 +180,9 @@
 		private Controls.ProductDataPanel ProductDataPanel;
 		private Shelves.GUI.Controls.ActionButton ResetActionButton;
 		private Shelves.GUI.Controls.ActionButton SaveActionButton;
+		private System.Windows.Forms.FlowLayoutPanel CrudPanelsFlowLayoutPanel;
+		private System.Windows.Forms.FlowLayoutPanel FlowLayoutPanel;
+		private Controls.PartsCrudPanel InventoryPartsCrudPanel;
+		private Controls.PartsCrudPanel AssociatedPartsCrudPanel;
 	}
 }
--- ShelvesApp/Common/GUI/Forms/ProductForm.cs
+++ ShelvesApp/Common/GUI/Forms/ProductForm.cs
@@ -9,13 +9,16 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 using Shelves.App.Common.GUI.Controls;
+using Shelves.BusinessLayer.Inventory;
+using Shelves.BusinessLayer.Parts.Abstract;
 using Shelves.BusinessLayer.Products;
-using Shelves.GUI.Forms;
 using Shelves.GUI.Colors;
+using Shelves.GUI.Controls.Entities;
+using Shelves.GUI.Forms;
 
 namespace Shelves.App.Common.GUI.Forms
 {
-	public partial class ProductForm : ActionForm
+	public partial class ProductForm : ActionFormResponsive
 	{
 		public ProductForm()
 		{
@@ -29,6 +32,80 @@ namespace Shelves.App.Common.GUI.Forms
 			set => ProductDataPanel.Product = value;
 		}
 
+
+		public override void DefineDynamicResizables()
+		{
+			var crudPanels = new List<UserControl> {
+				InventoryPartsCrudPanel,
+				AssociatedPartsCrudPanel
+			};
+
+			DynamicResizableControl crudPanelsContainer = new DynamicResizableControl(CrudPanelsFlowLayoutPanel, this);
+
+			crudPanelsContainer.AddResizingRule
+				(Dimension.Width,
+				width => FlowLayoutPanel.ClientSize.Width
+							- (	GetHorizontalMargin(CrudPanelsFlowLayoutPanel)
+								+ GetHorizontalPadding(CrudPanelsFlowLayoutPanel)
+								+ GetHorizontalMargin(FlowLayoutPanel)
+								+ GetHorizontalPadding(FlowLayoutPanel)),
+				SizeMode.Small);
+
+			crudPanelsContainer.AddResizingRule
+				(Dimension.Width,
+				width => FlowLayoutPanel.ClientSize.Width
+									- (ProductDataPanel.Width
+										+ GetHorizontalMargin(FlowLayoutPanel)
+										+ GetHorizontalPadding(FlowLayoutPanel)
+										+ GetHorizontalMargin(CrudPanelsFlowLayoutPanel)
+										+ GetHorizontalPadding(CrudPanelsFlowLayoutPanel)
+										+ 48),
+				SizeMode.Medium);
+
+			crudPanelsContainer.AddResizingRule
+				(Dimension.Width,
+				width => FlowLayoutPanel.ClientSize.Width
+									- (ProductDataPanel.Width
+										+ GetHorizontalMargin(FlowLayoutPanel)
+										+ GetHorizontalPadding(FlowLayoutPanel)
+										+ GetHorizontalMargin(CrudPanelsFlowLayoutPanel)
+										+ GetHorizontalPadding(CrudPanelsFlowLayoutPanel)
+										+ 48),
+				SizeMode.Full);
+
+			crudPanelsContainer.AddResizingRule
+				(Dimension.Height,
+				height => FlowLayoutPanel.ClientSize.Height
+							- (	GetVerticalPadding(CrudPanelsFlowLayoutPanel)
+								+ 48),
+				SizeMode.All);
+
+			foreach (var control in crudPanels)
+			{
+				DynamicResizableControl resizableControl = new DynamicResizableControl(control, this);
+
+				resizableControl.AddResizingRule
+					(Dimension.Width,
+						width => CrudPanelsFlowLayoutPanel.ClientSize.Width - 12,
+						SizeMode.All);
+
+				base.DynamicResizables.Add(resizableControl);
+			}
+
+			base.DynamicResizables.Add(crudPanelsContainer);
+
+			FitComponents();
+
+			PerformLayout();
+		}
+
+		public override void FitComponents()
+		{
+			if (Width > 1200) FitComponentsToFull();
+			else if (Width > 800) FitComponentsToMedium();
+			else FitComponentsToSmall();
+		}
+
 		protected override void Init()
 		{
 			base.Init();
@@ -39,8 +116,10 @@ namespace Shelves.App.Common.GUI.Forms
 			}
 		}
 
+
 		public void Reset() => ProductDataPanel.Reset();
 		public void ResetGui() => ProductDataPanel.ResetGui();
+
 
 		protected override void CloseButton_Click(object sender, EventArgs e)
 		{
@@ -55,6 +134,15 @@ namespace Shelves.App.Common.GUI.Forms
 		{
 			DialogResult = DialogResult.OK;
 			this.Hide();
+		}
+
+		private void ProductForm_Resize(object sender, EventArgs e) => ParentControl_Resize(sender, e);
+
+		private void ProductForm_Load(object sender, EventArgs e)
+		{
+			DefineDynamicResizables();
+
+			IList<Part> parts = Inventory.Instance.getParts();
 		}
 	}
 }
